460dc2aecfb3dc1269dd68ee4ba11ab4d92affb7
kejn <kaen04@gmail.com>
2016-03-26 00:10:09 +0100

    Teraz można oglądać także filmy z kategorii "Informacje i publicystyka"

--- content.js
+++ content.js
@@ -2,14 +2,21 @@
 	var iframe = document.querySelector('.movieWrapper > iframe');
 	
 	if(iframe === undefined || iframe === null) {
-		// Couldn't find IFRAME inside .movieWrapper element
+		console.error('Couldn\'t find IFRAME inside .movieWrapper element');
 		throw new Error('Nie jesteś na stronie z filmem.');
 	}
-	
-	iframe = iframe.contentDocument || iframe.contentWindow.document;
-	var url = iframe.documentElement.innerHTML.match(/\'http:\/\/(.*)type\: \'video\//g);
+	console.log(iframe.src);
+	if(iframe.src.indexOf('vod.tvp.pl') < 0) {
+		throw new Error('src;' + iframe.src);
+	}
+	var iframeContent = iframe.contentWindow.document || iframe.contentDocument ;
+	return searchInIframe(iframeContent.documentElement.innerHTML);
+}
+
+function searchInIframe(iframeContent) {
+	var url = iframeContent.match(/\'http:\/\/(.*)type\: \'video\//g);
 	if(url === undefined || url === null) {
-		// Silverlight
+		console.error('Silverlight');
 		throw new Error('Ta wtyczka nie generuje linków dla filmów obsługiwanych przez Silverlight');
 	}
 	url = url.toString().split("\'")[1];
@@ -24,13 +31,33 @@ var result;
 
 try {
 	result = findMovieUrl();
-} catch(err) {
-	result = {
-		'movieUrl': null,
-		'error': err.message
-	}
-	console.error(result.error);
-} finally {
 	chrome.runtime.sendMessage(result);
+} catch(err) {
+	try {
+		if(err.message.indexOf('src') != 0) {
+			throw err;
+		}
+		console.log('sending cross domain origin request');
+		var iframeSource = err.message.split(';')[1];
+		var request = new XMLHttpRequest();
+		request.open('GET',iframeSource);
+		request.responseType = 'text';
+		request.onreadystatechange = function() {
+			if (request.readyState==4 && request.status==200)
+        {
+					var remoteIframeContent = request.responseText;
+					result = searchInIframe(remoteIframeContent);
+					chrome.runtime.sendMessage(result);
+        }
+		}
+		request.send();
+	} catch(err2) {
+		result = {
+			'movieUrl': null,
+			'error': err2.message
+		}
+		console.error(result.error);
+		chrome.runtime.sendMessage(result);
+	}
 }
 
